2b7ccb9d053847b448da18044838fc1fcfe76ba8
Ahmed11711 <Ahmed11711@gmail.com>
2025-12-10 03:17:59 -0800

    aa

--- app/Http/Controllers/Api/HelperForFront/ApiHelperFrontController.php
+++ app/Http/Controllers/Api/HelperForFront/ApiHelperFrontController.php
@@ -10,18 +10,26 @@ use App\Http\Controllers\Controller;
 
 class ApiHelperFrontController extends Controller
 {
-    use ApiResponseTrait;
-    public function getGrades(Request $request)
-    {
-        $grades = Grade::with('subjects:id,name,grade_id')->get();
-        return $this->successResponse($grades, "Grades with Subjects");
-    }
+ use ApiResponseTrait;
+ public function getGrades(Request $request)
+ {
+  $grades = Grade::with('subjects:id,name,grade_id')->get();
+  return $this->successResponse($grades, "Grades with Subjects");
+ }
 
-    public function allTeacherAndSchool()
-    {
-        $users = User::teachersAndStudents()
-            ->select('id', 'username', 'email', 'role')
-            ->get();
-        return $this->successResponse($users, 'Teachers and Students');
-    }
+ public function allTeacherAndSchool()
+ {
+  $teachers = User::where('role', 'teacher')
+   ->select('id', 'username', 'email', 'role')
+   ->get();
+
+  $schools = User::where('role', 'school')
+   ->select('id', 'username', 'email', 'role')
+   ->get();
+
+  return $this->successResponse([
+   'teachers' => $teachers,
+   'schools'  => $schools,
+  ], 'Teachers and Schools');
+ }
 }
