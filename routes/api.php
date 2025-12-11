85f2ed457b3d9ec6f7b5bfa435e4736d04f29185
Ahmed11711 <Ahmed11711@gmail.com>
2025-12-11 07:55:03 -0800

    aa

--- routes/api.php
+++ routes/api.php
@@ -11,15 +11,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1/')->group(function () {
 
- Route::prefix('Auth/')->group(function () {
-
-  Route::post('create-account', [CreateAccountController::class, 'createAccount']);
- });
-
  Route::prefix('auth/')->group(function () {
 
+  Route::post('create-account', [CreateAccountController::class, 'createAccount']);
   Route::post('login', [LoginController::class, 'login']);
  });
+
 
 
 
