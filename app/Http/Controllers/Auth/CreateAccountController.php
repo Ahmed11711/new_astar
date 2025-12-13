6ed7b3e38209f1736fb3da3f5196c383b2610331
Ahmed11711 <Ahmed11711@gmail.com>
2025-12-13 02:39:10 -0800

    aaa

--- app/Http/Controllers/Auth/CreateAccountController.php
+++ app/Http/Controllers/Auth/CreateAccountController.php
@@ -17,17 +17,16 @@ class CreateAccountController extends Controller
  public function createAccount(CreateAccountRequest $request)
  {
   return DB::transaction(function () use ($request) {
-   $user = User::create([
-    'username'     => $request->username,
-    'first_name'   => $request->first_name,
-    'last_name'    => $request->last_name,
-    'email'        => $request->email,
-    'role'         => $request->role,
-    'student_type' => $request->student_type,
-    'phone'        => $request->phone,
-    'password'     => $request->password,
-    'is_active'    => true,
-   ]);
+   $user = User::create($request->only([
+    'username',
+    'first_name',
+    'last_name',
+    'email',
+    'role',
+    'student_type',
+    'phone'
+
+   ]) + ['is_active' => true, 'password' => Hash::make($request->password ?? '123456')]);
 
    if ($user->role === 'student') {
     $assignedId = $request->school_id ?? $request->teacher_id;
