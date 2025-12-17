579537ae2aa893bc76d665e297c1b2c5b9b56ae6
Ahmed11711 <ahmed11711@gmail.com>
2025-12-17 17:12:04 +0200

    paper-type

--- app/Http/Service/ExamPaperService.php
+++ app/Http/Service/ExamPaperService.php
@@ -9,77 +9,77 @@ use Illuminate\Support\Facades\DB;
 
 class ExamPaperService
 {
- public function createExamPaperWithQuestions($data)
- {
-  // return $data;
-  return DB::transaction(function () use ($data) {
-   $paper = ExamPaper::create([
-    'subject_id'       => $data['subject_id'],
-    'grade_id'         => $data['grade_id'],
-    'paper_id'         => $data['paper_id'] ?? null,
-    'title'            => $data['title'],
-    'paper_label'      => $data['paper_label'] ?? null,
-    'year'             => $data['year'] ?? null,
-    'month'            => $data['month'] ?? null,
-    'is_active'        => $data['is_active'] ?? true,
-    'total_marks'      => $data['total_marks'] ?? null,
-    'duration_minutes' => $data['duration_minutes'] ?? null,
-    'meta'             => $data['meta'] ?? [],
-   ]);
+    public function createExamPaperWithQuestions($data)
+    {
+        // return $data;
+        return DB::transaction(function () use ($data) {
+            $paper = ExamPaper::create([
+                'subject_id'       => $data['subject_id'],
+                'grade_id'         => $data['grade_id'],
+                'paper_id'         => $data['paper_type'] ?? null,
+                'title'            => $data['title'],
+                'paper_label'      => $data['paper_label'] ?? null,
+                'year'             => $data['year'] ?? null,
+                'month'            => $data['month'] ?? null,
+                'is_active'        => $data['is_active'] ?? true,
+                'total_marks'      => $data['total_marks'] ?? null,
+                'duration_minutes' => $data['duration_minutes'] ?? null,
+                'meta'             => $data['meta'] ?? [],
+            ]);
 
-   $this->insertQuestions(
-    $paper->id,
-    $data['questions'],
-    null,
-    $paper->subject_id,
-    $data['topic'] ?? null
-   );
+            $this->insertQuestions(
+                $paper->id,
+                $data['questions'],
+                null,
+                $paper->subject_id,
+                $data['topic'] ?? null
+            );
 
-   return $paper->load('questions');
-  });
- }
-
- private function insertQuestions($paperId, $questions, $parentId = null, $subjectId = null, $topicId = null)
- {
-  foreach ($questions as $q) {
-
-   $question = Question::create([
-    'exam_paper_id'      => $paperId,
-    'subject_id'         => $subjectId,
-    'topic_id'           => $q['topic_id'],
-    'subtopics_id'       => $q['subtopics_id'],
-    'question_type'      => $q['question_type'],
-    'question_string'    => $q['question_string'] ?? null,
-    'question_number'    => $q['question_number'],
-    'question_max_score' => $q['question_max_score'] ?? null,
-    'marking_scheme'     => $q['marking_scheme'] ?? [],
-    'has_options'        => !empty($q['options']),
-    'parent_id' => $parentId,
-   ]);
-
-   // insert options
-   if (!empty($q['options'])) {
-    $opts = [];
-    foreach ($q['options'] as $o) {
-     $opts[] = [
-      'question_id' => $question->id,
-      'text'        => $o['text'],
-      'is_correct'  => $o['is_correct'],
-     ];
+            return $paper->load('questions');
+        });
     }
-    QuestionOption::insert($opts);
-   }
 
-   // recursive for sub-questions
-   if (!empty($q['sub_questions'])) {
-    $this->insertQuestions(
-     $paperId,
-     $q['sub_questions'],
-     $question->id,
-     $subjectId,
-     $topicId
-    );
-   }
-  }
- }
+    private function insertQuestions($paperId, $questions, $parentId = null, $subjectId = null, $topicId = null)
+    {
+        foreach ($questions as $q) {
+
+            $question = Question::create([
+                'exam_paper_id'      => $paperId,
+                'subject_id'         => $subjectId,
+                'topic_id'           => $q['topic_id'],
+                'subtopics_id'       => $q['subtopics_id'],
+                'question_type'      => $q['question_type'],
+                'question_string'    => $q['question_string'] ?? null,
+                'question_number'    => $q['question_number'],
+                'question_max_score' => $q['question_max_score'] ?? null,
+                'marking_scheme'     => $q['marking_scheme'] ?? [],
+                'has_options'        => !empty($q['options']),
+                'parent_id' => $parentId,
+            ]);
+
+            // insert options
+            if (!empty($q['options'])) {
+                $opts = [];
+                foreach ($q['options'] as $o) {
+                    $opts[] = [
+                        'question_id' => $question->id,
+                        'text'        => $o['text'],
+                        'is_correct'  => $o['is_correct'],
+                    ];
+                }
+                QuestionOption::insert($opts);
+            }
+
+            // recursive for sub-questions
+            if (!empty($q['sub_questions'])) {
+                $this->insertQuestions(
+                    $paperId,
+                    $q['sub_questions'],
+                    $question->id,
+                    $subjectId,
+                    $topicId
+                );
+            }
+        }
+    }
 }
