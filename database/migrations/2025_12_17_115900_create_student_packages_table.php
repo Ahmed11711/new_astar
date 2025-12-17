627cb0f5f1afb116d88db7e1f37429f90888fd0a
Ahmed11711 <ahmed11711@gmail.com>
2025-12-17 14:00:24 +0200

    StudentPackage

---
+++ database/migrations/2025_12_17_115900_create_student_packages_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('student_packages', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('student_id');
+            $table->unsignedBigInteger('package_id');
+
+            $table->date('starts_at');
+            $table->date('ends_at')->nullable();
+            $table->boolean('active')->default(true);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('student_packages');
+    }
+};
