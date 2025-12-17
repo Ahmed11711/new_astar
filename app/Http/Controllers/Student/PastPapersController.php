36574fe95af69498eb4b4caf087e9ab419523352
Ahmed11711 <ahmed11711@gmail.com>
2025-12-17 16:38:35 +0200

    past-papers

---
+++ app/Http/Controllers/Student/PastPapersController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Student;
+
+use App\Http\Controllers\Controller;
+use App\Models\ExamPaper;
+use App\Models\paper;
+use Illuminate\Http\Request;
+
+class PastPapersController extends Controller
+{
+    public function index()
+    {
+
+        $papers = paper::get();
+        return $papers;
+    }
+}
--- routes/student.php
+++ routes/student.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\Student\PastPapersController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::prefix('v1/student/')->group(function () {});
+
+Route::prefix('v1/student/')->group(function () {
+    Route::get('past-papers', [PastPapersController::class, 'index']);
+});
