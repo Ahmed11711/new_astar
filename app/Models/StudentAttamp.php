3c97d509a131ad544b54d7e87488269c3ffd78ce
Ahmed11711 <ahmed11711@gmail.com>
2025-12-22 14:32:38 +0200

    ss

--- app/Models/StudentAttamp.php
+++ app/Models/StudentAttamp.php
@@ -13,8 +13,14 @@ class StudentAttamp extends Model
         return $this->belongsTo(ExamPaper::class, 'exam_id');
     }
 
-    public function answers()
+    public function سس()
     {
         return $this->hasMany(answer::class, 'attempt_id');
+    }
+    public function answers()
+    {
+        return $this->hasOne(Answer::class, 'question_id')
+            ->where('user_id', auth()->id() ?? null) // لو حابب فلتر على الطالب الحالي
+            ->latest('created_at'); // آخر إجابة حسب وقت الإنشاء
     }
 }
