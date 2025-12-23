aac59a783ed8cfb950d3d4cb6978fc768315f653
Ahmed11711 <ahmed11711@gmail.com>
2025-12-23 15:04:46 +0200

    lable

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('run-migrate', function () {
     //
-    Artisan::call('migrate:fresh', ['--force' => true]);
+    Artisan::call('migrate:refresh', ['--force' => true]);
     Artisan::call('db:seed', [
         '--force' => true,
     ]);
